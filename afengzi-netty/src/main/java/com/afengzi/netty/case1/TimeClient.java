d14f57c07e935832b93cfeba26da1af80cf8f73c
klov <xhli110@126.com>
2015-11-24 22:43:00 +0800

    hello netty

---
+++ afengzi-netty/src/main/java/com/afengzi/netty/case1/TimeClient.java
@@ -0,0 +1,35 @@
+package com.afengzi.netty.case1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by klov on 2015/11/24.
+ */
+public class TimeClient {
+    public void connect(int port, String host) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect(host, port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    private class TimeClientHandler extends ChannelHandlerAdapter {
+    }
+}
