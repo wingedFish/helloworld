840cea81fab9c62a58cf83034870b9ebf760e817
klov <xhli110@126.com>
2015-11-26 22:39:34 +0800

    hello netty

--- afengzi-netty/src/main/java/com/afengzi/netty/case1/TimeClient.java
+++ afengzi-netty/src/main/java/com/afengzi/netty/case1/TimeClient.java
@@ -1,6 +1,9 @@
 package com.afengzi.netty.case1;
 
+import com.afengzi.util.LoggerHelper;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -10,6 +13,14 @@ import io.netty.channel.socket.nio.NioSocketChannel;
  * Created by klov on 2015/11/24.
  */
 public class TimeClient {
+
+    private byte[] req;
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+
     public void connect(int port, String host) throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
@@ -31,5 +42,39 @@ public class TimeClient {
     }
 
     private class TimeClientHandler extends ChannelHandlerAdapter {
+        private final ByteBuf firstMessage;
+        private int counter;
+
+        public TimeClientHandler() {
+            req = "QUERY TIME ORDER".getBytes();
+            firstMessage = Unpooled.buffer(req.length);
+            firstMessage.writeBytes(req);
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            ByteBuf message = null;
+            for (int i = 0; i < 100; i++) {
+                message = Unpooled.buffer(req.length);
+                message.writeBytes(message);
+                ctx.writeAndFlush(firstMessage);
+            }
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            byte[] req = new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(req);
+            String body = new String(req, "UTF-8");
+            LoggerHelper.print("Now is : " + body+" , the counter is : "+ ++counter);
+
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            LoggerHelper.print("Unexpected exception from downstream : " + cause.getMessage());
+            ctx.close();
+        }
     }
 }
--- afengzi-netty/src/main/java/com/afengzi/netty/case1/TimeServerHandler.java
+++ afengzi-netty/src/main/java/com/afengzi/netty/case1/TimeServerHandler.java
@@ -12,14 +12,16 @@ import java.util.Date;
  * Created by klov on 2015/11/24.
  */
 public class TimeServerHandler extends ChannelHandlerAdapter {
+    private int counter ;
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-        String body = new String(req, "UTF-8");
-        LoggerHelper.print("the time server receive order : " + body);
+        String body = new String(req, "UTF-8").substring(0,System.getProperty("line.separator").length());
+        LoggerHelper.print("the time server receive order : " + body+" , the counter is : "+ ++counter);
         String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD ORDER";
+        currentTime = currentTime+System.getProperty("line.separator");
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
         ctx.write(resp);
     }
