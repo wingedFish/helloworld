e08bd7cd9d66a20808a27f28c5cd96e9c14f7c76
xhli110@126.com <xhli110@126.com>
2016-03-08 17:36:41 +0800

    线程池监控

---
+++ afengzi-concurrent/src/main/java/com/afengzi/concurrent/product/threadPoolMonitor/ThreadPoolNameEnum.java
@@ -0,0 +1,31 @@
+package com.afengzi.concurrent.product.threadPoolMonitor;
+
+/**
+ * Created by lixiuhai on 2016/3/8.
+ */
+public enum ThreadPoolNameEnum {
+
+    EatMeat("eatMeatThreadPool","1","eat meat");
+
+    private String threadPoolName;
+    private String threadPoolType;
+    private String threadPoolDesc;
+
+    ThreadPoolNameEnum(String threadPoolName,String threadPoolDesc, String threadPoolType) {
+        this.threadPoolName = threadPoolName;
+        this.threadPoolDesc = threadPoolDesc;
+        this.threadPoolType = threadPoolType;
+    }
+
+    public String getThreadPoolDesc() {
+        return threadPoolDesc;
+    }
+
+    public String getThreadPoolName() {
+        return threadPoolName;
+    }
+
+    public String getThreadPoolType() {
+        return threadPoolType;
+    }
+}
