6bb95e291736a187c1a5941600c860adf4d9fce9
xhli110@126.com <xhli110@126.com>
2016-05-21 15:01:53 +0800

    Squashed commit of the following:
    commit cd24d2967bd56fa8f1acf07ec609d525033d41e0
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Thu Apr 21 16:15:40 2016 +0800
    
        新增Hystrix
    
    commit 7d06426faa0cc44d9d380fdcb5341d9e4e8c8084
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Tue Apr 19 16:44:21 2016 +0800
    
        新增akka
    
    commit e08bd7cd9d66a20808a27f28c5cd96e9c14f7c76
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Tue Mar 8 17:36:41 2016 +0800
    
        线程池监控

--- afengzi-netflix-hystrix/src/main/java/com/afengzi/hystrix/HelloworldCommand.java
+++ afengzi-netflix-hystrix/src/main/java/com/afengzi/hystrix/HelloworldCommand.java
@@ -2,6 +2,9 @@ package com.afengzi.hystrix;
 
 import com.netflix.hystrix.*;
 import com.netflix.hystrix.strategy.properties.HystrixPropertiesCommandDefault;
+import rx.Observable;
+import rx.Observer;
+import rx.util.functions.Action1;
 
 /**
  * Created by lixiuhai on 2016/4/24.
@@ -10,21 +13,24 @@ public class HelloworldCommand extends HystrixCommand<String> {
 
     private String name ;
 
+
+
     protected HelloworldCommand(String name) {
         super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("hello"))
                 .andCommandKey(HystrixCommandKey.Factory.asKey("hello" + name))
                 .andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("hello$Pool" + name))
                 .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
-                        .withCoreSize(10)
+                        .withCoreSize(15)
                         .withMaxQueueSize(10)
-                        .withQueueSizeRejectionThreshold(20))
+                        .withQueueSizeRejectionThreshold(2))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionIsolationStrategy(HystrixCommandProperties.ExecutionIsolationStrategy.THREAD))
                 .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withCircuitBreakerSleepWindowInMilliseconds(5000)));
         this.name = name;
     }
 
     @Override
     protected String run() throws Exception {
-        Thread.sleep(3000);
+        Thread.sleep(300);
         System.out.println("run success "+Thread.currentThread().getName());
         return "run success "+Thread.currentThread().getName();
     }
@@ -35,12 +41,49 @@ public class HelloworldCommand extends HystrixCommand<String> {
         return "fall back..."+Thread.currentThread().getName();
     }
 
-    public static void main(String[] args) {
-        for (int i = 0 ; i < 100 ; i++){
+    private static void reactiveExecution(){
+        Observable<String> observable = new HelloworldCommand("xhl").toObservable();
+        observable.subscribe(new Action1<String>() {
+            //first call
+            @Override
+            public void call(String s) {
+                System.out.println("reactiveExecution call ..."+Thread.currentThread().getName());
+            }
+        });
+        observable.subscribe(new Observer<String>() {
+            //third call
+            @Override
+            public void onCompleted() {
+                System.out.println("reactiveExecution onCompleted ..."+Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("reactiveExecution onError ..."+Thread.currentThread().getName());
+            }
+
+            //second call
+            @Override
+            public void onNext(String s) {
+                System.out.println("reactiveExecution onNext ..."+Thread.currentThread().getName());
+            }
+        });
+    }
+
+    private static void synExecution(){
             HelloworldCommand helloworldCommand = new HelloworldCommand("KLOV");
             helloworldCommand.queue();
-//            String result = helloworldCommand.execute();
+            String result = helloworldCommand.execute();
 //            System.out.println(i+" result ="+result);
+    }
+
+
+    public static void main(String[] args) {
+        for (int i = 0 ; i< 15 ; i++){
+//            reactiveExecution();
+            HelloworldCommand helloworldCommand = new HelloworldCommand("KLOV");
+            helloworldCommand.execute();
+//            String result = helloworldCommand.execute();
         }
     }
 }
