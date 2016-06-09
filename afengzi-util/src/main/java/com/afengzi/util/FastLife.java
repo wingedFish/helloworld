1eccfc24412d074e72603dc8e48883cb90ae58d9
xhli110@126.com <xhli110@126.com>
2016-06-09 14:27:45 +0800

    Squashed commit of the following:
    commit 6bb95e291736a187c1a5941600c860adf4d9fce9
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Sat May 21 15:01:53 2016 +0800
    
        Squashed commit of the following:
    
        commit cd24d2967bd56fa8f1acf07ec609d525033d41e0
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Thu Apr 21 16:15:40 2016 +0800
    
            新增Hystrix
    
        commit 7d06426faa0cc44d9d380fdcb5341d9e4e8c8084
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Apr 19 16:44:21 2016 +0800
    
            新增akka
    
        commit e08bd7cd9d66a20808a27f28c5cd96e9c14f7c76
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Mar 8 17:36:41 2016 +0800
    
            线程池监控
    
    commit 5fc6a61ddcd54fe582d9699fd242a8ae7f7de82c
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Wed Apr 27 18:20:12 2016 +0800
    
        Squashed commit of the following:
    
        commit cd24d2967bd56fa8f1acf07ec609d525033d41e0
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Thu Apr 21 16:15:40 2016 +0800
    
            新增Hystrix
    
        commit 7d06426faa0cc44d9d380fdcb5341d9e4e8c8084
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Apr 19 16:44:21 2016 +0800
    
            新增akka
    
        commit e08bd7cd9d66a20808a27f28c5cd96e9c14f7c76
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Mar 8 17:36:41 2016 +0800
    
            线程池监控
    
    commit 9c8293ded5d0da980a38aa3abba2a8bfa4ce2c82
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Sun Apr 24 22:38:51 2016 +0800
    
        Squashed commit of the following:
    
        commit cd24d2967bd56fa8f1acf07ec609d525033d41e0
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Thu Apr 21 16:15:40 2016 +0800
    
            新增Hystrix
    
        commit 7d06426faa0cc44d9d380fdcb5341d9e4e8c8084
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Apr 19 16:44:21 2016 +0800
    
            新增akka
    
        commit e08bd7cd9d66a20808a27f28c5cd96e9c14f7c76
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Mar 8 17:36:41 2016 +0800
    
            线程池监控
    
    commit 9edab0b45905a9f9bf57c5a86079fd43c089cd24
    Merge: 6e342ff cd24d29
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Sun Apr 24 21:47:19 2016 +0800
    
        Merge remote-tracking branch 'origin/master'
    
    commit 6e342ff3ddf3681777e455c805b213ad874d4822
    Author: xhli110@126.com <xhli110@126.com>
    Date:   Sun Apr 24 21:47:03 2016 +0800
    
        Squashed commit of the following:
    
        commit cd24d2967bd56fa8f1acf07ec609d525033d41e0
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Thu Apr 21 16:15:40 2016 +0800
    
            新增Hystrix
    
        commit 7d06426faa0cc44d9d380fdcb5341d9e4e8c8084
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Apr 19 16:44:21 2016 +0800
    
            新增akka
    
        commit e08bd7cd9d66a20808a27f28c5cd96e9c14f7c76
        Author: xhli110@126.com <xhli110@126.com>
        Date:   Tue Mar 8 17:36:41 2016 +0800
    
            线程池监控

---
+++ afengzi-util/src/main/java/com/afengzi/util/FastLife.java
@@ -0,0 +1,69 @@
+package com.afengzi.util;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by lixiuhai on 2016/6/9.
+ */
+public class FastLife {
+    private final static Logger log = LogManager.getLogger(FastLife.class);
+    private String secretKey = "2099lixiuhai2213";//加解密密钥
+    private String charetSet = "utf-16";
+
+    public String encryptStrWithAes(String str) {
+        String resStr = null;
+        try {
+            byte[] raw = secretKey.getBytes();
+            System.out.println(raw.length);
+            SecretKeySpec key = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");//"算法/模式/补码方式"
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] byteContent = str.getBytes(charetSet);
+            byte[] encrypted = cipher.doFinal(byteContent);
+            BASE64Encoder encoder = new BASE64Encoder();
+            resStr = encoder.encode(encrypted);
+        }
+        catch (Exception e) {
+            log.error("AES加密失败", e);
+            System.out.println(e);
+        }
+        return resStr;
+    }
+    /**
+     * AES解密
+     * @param str
+     * @return
+     */
+    public String decryptStrWithAes(String str) {
+        String resStr = null;
+        try {
+            byte[] raw = secretKey.getBytes();
+            SecretKeySpec key = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] decrypted = cipher.doFinal(decoder.decodeBuffer(str));
+            resStr = new String(decrypted, charetSet);
+        }
+        catch (Exception e) {
+            log.error("AES解密失败", e);
+            System.out.println(e);
+        }
+        return resStr;
+    }
+
+    public static void main(String[] args) {
+        FastLife fastLife = new FastLife();
+        String after = fastLife.encryptStrWithAes("");
+        System.out.println("after  "+after);
+
+        String before = fastLife.decryptStrWithAes(after) ;
+        System.out.println("before " + before);
+    }
+}
